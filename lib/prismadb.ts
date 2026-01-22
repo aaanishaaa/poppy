245d4cfcb0b3346419c4c5cfec6bc0ec753f083b
Anisha <anishaworks21@gmail.com>
2026-01-22 23:15:48 +0530

    Prisma initiated

---
+++ lib/prismadb.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+declare global{
+    var prisma: PrismaClient|undefined
+};
+
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+const prismadb = globalThis.prisma ||new PrismaClient({ adapter });
+if(process.env.NODE_ENV!=='production') globalThis.prisma=prismadb;
+
+export default prismadb;
